4addedf9f8573226248132628a47de27029cc212
Estefania Muñoz <157163383+FannaMC@users.noreply.github.com>
2024-04-25 15:26:55 -0600

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,12 @@
+//Haz tú validación en javascript acá
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity(
+      "¡Se esperaba una dirección de correo electrónico!",
+    );
+  } else {
+    email.setCustomValidity("");
+  }
+});
